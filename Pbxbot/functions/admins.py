e0f4e2a613d7d7876f07394e0db1304d4131ec28
lucifersparklesoul <lucifersparkle891@gmail.com>
2026-06-09 17:24:28 +0530

    Add admin management functions for chat

---
+++ Pbxbot/functions/admins.py
@@ -0,0 +1,24 @@
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
+from pyrogram.types import Chat
+
+from Pbxbot.core import Pbxbot
+
+
+async def get_admins(chat_id: int) -> list:
+    admins = []
+    async for x in Pbxbot.bot.get_chat_members(
+        chat_id, filter=ChatMembersFilter.ADMINISTRATORS
+    ):
+        admins.append(x.user.id)
+    return admins
+
+
+async def is_user_admin(chat: Chat, user_id: int) -> bool:
+    if chat.type in [ChatType.PRIVATE, ChatType.BOT]:
+        return True
+
+    status = (await chat.get_member(user_id)).status
+    if status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        return True
+
+    return False
